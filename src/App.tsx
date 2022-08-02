0a87673fef8e426313dd7288a1e08d36b1b1e118
Igor Siomushkin <semajigr@gmail.com>
2022-08-02 16:34:29 +0300

    feat: add homework-1

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Slider } from "./components/Slider/Slider";
+import "./app.scss";
+
+export const App = () => {
+  return <div className="app"></div>;
+};
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./navbar.scss";
+import logo from "./logo.svg";
+
+export const Navbar = () => {
+  return <nav className="nav"></nav>;
+};
