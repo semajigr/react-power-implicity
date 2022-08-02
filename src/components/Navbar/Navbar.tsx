738eb428a5674e061ad7353804b38b9288f58a77
Igor Siomushkin <semajigr@gmail.com>
2022-08-02 16:36:36 +0300

    feat: add content

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,5 +3,29 @@ import "./navbar.scss";
 import logo from "./logo.svg";
 
 export const Navbar = () => {
-  return <nav className="nav"></nav>;
+  return (
+    <nav className="nav">
+      <a href="/" className="nav__link">
+        <img src={logo} alt="logo" className="nav__logo" />
+      </a>
+      <ul className="nav__list">
+        <li className="nav__item">
+          <a href="/" className="nav__item-list">
+            Features
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/" className="nav__item-list">
+            Partners
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/" className="nav__item-list">
+            Stories
+          </a>
+        </li>
+      </ul>
+      <button className="nav__button">Download for free</button>
+    </nav>
+  );
 };
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -2,5 +2,23 @@ import React from "react";
 import "./slider.scss";
 
 export const Slider = () => {
-  return <div className="slider"></div>;
+  return (
+    <div className="slider">
+      <div className="slider__container">
+        <h2 className="slider__title">The Power of Simplicity</h2>
+        <p className="slider__description">
+          Instead of spending time searching for the right app, our AI will
+          bring the right app to you.
+        </p>
+        <button className="slider__button">Learn</button>
+        <div className="slider__dots">
+          <a href="/#" className="slider__dot"></a>
+          <a href="/#" className="slider__dot"></a>
+          <a href="/#" className="slider__dot--active"></a>
+          <a href="/#" className="slider__dot"></a>
+          <a href="/#" className="slider__dot"></a>
+        </div>
+      </div>
+    </div>
+  );
 };
