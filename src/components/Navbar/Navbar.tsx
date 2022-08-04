aafb69fdd322f7ccf445f5ba9b7fba0822e429cb
Igor Siomushkin <semajigr@gmail.com>
2022-08-04 12:18:26 +0300

    fix: some bugs

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./navbar.scss";
-import logo from "./logo.svg";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 export const Navbar = () => {
   return (
     <nav className="nav">
       <a href="/" className="nav__link">
-        <img src={logo} alt="logo" className="nav__logo" />
+        <Logo className="nav__logo" />
       </a>
       <ul className="nav__list">
         <li className="nav__item">
@@ -25,7 +25,9 @@ export const Navbar = () => {
           </a>
         </li>
       </ul>
-      <button className="nav__button">Download for free</button>
+      <button className="nav__button" type="button">
+        Download for free
+      </button>
     </nav>
   );
 };
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./slider.scss";
+import button from "../../assets/button.png";
 
 export const Slider = () => {
   return (
@@ -10,14 +11,16 @@ export const Slider = () => {
           Instead of spending time searching for the right app, our AI will
           bring the right app to you.
         </p>
-        <button className="slider__button">Learn</button>
-        <div className="slider__dots">
-          <a href="/#" className="slider__dot"></a>
-          <a href="/#" className="slider__dot"></a>
-          <a href="/#" className="slider__dot--active"></a>
-          <a href="/#" className="slider__dot"></a>
-          <a href="/#" className="slider__dot"></a>
-        </div>
+        <button className="slider__button" type="button">
+          <img src={button} alt="button" />
+        </button>
+        <ul className="slider__dots">
+          <li className="slider__dot"></li>
+          <li className="slider__dot"></li>
+          <li className="slider__dot slider__dot--active"></li>
+          <li className="slider__dot"></li>
+          <li className="slider__dot"></li>
+        </ul>
       </div>
     </div>
   );
